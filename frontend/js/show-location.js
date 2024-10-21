978d9b21c00cba7a64621aada847aa25b6297a32
Ironboy <ironboy@users.noreply.github.com>
2024-10-21 22:58:43 +0200

    color layout background "ambient light"

--- frontend/js/show-location.js
+++ frontend/js/show-location.js
@@ -5,9 +5,22 @@ function showLocation(loc, alternateDescription, alternateImage) {
   // else use the normal ones provided for the location
   let image = alternateImage || loc.image;
   let description = alternateDescription || loc.description;
+
+  // tone in img
+  if (image !== window.lastImage) {
+    $('.viewport-bg').els[0].style.backgroundImage = `url("imgs/${image}.jpg")`;
+    let alpha = 1;
+    (function toneInBg() {
+      $('.backdropper').els[0].style.backgroundColor = `rgba(0,0,0,${alpha})`
+      alpha > 0.6 && (alpha -= 0.02) && setTimeout(toneInBg, 50);
+    })();
+  }
+  window.lastImage = image;
+
   // Write to elements
   $('.big-image').attr("src", "imgs/" + image + ".jpg");
   $('.description').html(description.replaceAll(' - ', ' &ndash; '));
+
   // If a location is not an info page then change the memories
   // holding the last location and last description
   if (!loc.infopage) {
